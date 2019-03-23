3b41a4233de6f1864e252d9c3c44294fea77dfdd
balraj <erbalrajs@gmail.com>
2019-03-23 13:31:42 +0530

    Added iOS Project and added a pod file

---
+++ LiveStreamFails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveStreamFails
+//
+//  Created by Balraj Singh on 23/03/19.
+//  Copyright © 2019 balraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-LiveStreamFails/Pods-LiveStreamFails-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiveStreamFails : NSObject
+@end
+@implementation PodsDummy_Pods_LiveStreamFails
+@end
